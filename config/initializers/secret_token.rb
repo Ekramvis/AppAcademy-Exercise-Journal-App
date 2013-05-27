b864c212c715a93ada15ec386d8797888b0b145e
James and Scott <James+Scott@appacademy.io>
2013-05-27 09:21:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalApp::Application.config.secret_token = '0905c295277e9f4a3ad41ad3f36eec576e8dd4a15f6405baa920aef579e940b0f68e36c4d0c69b24d55beed5ec0e017d84b5590f1476b2401fd61f1640dd9795'
---
+++ db/migrate/20130527132001_create_entries.rb
@@ -0,0 +1,7 @@
+class CreateEntries < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
